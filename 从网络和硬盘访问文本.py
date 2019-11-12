2ebf265e1aa363ebb41edc0762407ed538c9619e
Tang4109 <2298964704@qq.com>
2019-11-12 17:16:28 +0800

    从网络爬取文档并转为str类型

--- 从网络和硬盘访问文本.py
+++ 从网络和硬盘访问文本.py
@@ -7,19 +7,28 @@
 @Software: PyCharm
 '''
 
-import warnings
+from __future__ import division
+import nltk,re,pprint
+import urllib
+from urllib import request
+from urllib.request import urlopen
+url='http://www.gutenberg.org/files/58700/58700-0.txt'
+# proxy={'http':'http://www.someproxy.com:3128'}
+#如果不加上下面的这行出现会出现urllib2.HTTPError: HTTP Error 403: Forbidden错误
+#主要是由于该网站禁止爬虫导致的，可以在请求加上头信息，伪装成浏览器访问User-Agent,具体的信息可以通过火狐的FireBug插件查询
+# headers = {'User-Agent':'Mozilla/5.0 (Windows NT 6.1; WOW64; rv:23.0) Gecko/20100101 Firefox/23.0'}
+# req = urllib.request.Request(url=url, headers=headers)
+# raw = urllib.request.urlopen(req).read()
+response=urlopen(url)
+raw = response.read().decode('utf-8')  # 返回的数据格式为bytes类型，需要decode()解码，转换成str类型
+print(type(raw))
+print(len(raw))
+print(raw[:75])
 
-warnings.filterwarnings('ignore')
-import numpy as np
-import pandas as pd
-from sklearn.datasets import load_breast_cancer, load_iris, make_blobs
-from sklearn.model_selection import train_test_split, cross_val_score, KFold, LeaveOneOut, \
-    ShuffleSplit, StratifiedShuffleSplit, GroupKFold, GridSearchCV
-from sklearn.preprocessing import MinMaxScaler, OneHotEncoder, PolynomialFeatures
-from sklearn.ensemble import RandomForestClassifier
-from sklearn.linear_model import LogisticRegression, LinearRegression, Ridge
-from sklearn.feature_selection import RFE
-from sklearn.metrics import mean_squared_error
-from sklearn.svm import SVC
-import matplotlib.pyplot as plt
-import mglearn
+tokens=nltk.word_tokenize(raw)
+print(type(tokens))
+print(len(tokens))
+print(tokens[:10])
+
+
+
