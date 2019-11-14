0147fed8e3b210b11b729c044a8e72bf49a0326e
Tang4109 <2298964704@qq.com>
2019-11-14 09:17:43 +0800

    使用正则表达式检测词组搭配

--- 3.5 正则表达式的有益应用.py
+++ 3.5 正则表达式的有益应用.py
@@ -19,18 +19,29 @@ wsj = sorted(set(nltk.corpus.treebank.words()))
 fd = nltk.FreqDist(vs for word in wsj for vs in re.findall(r'[aeiou]{2,}', word))
 print(fd.items())
 
+# 2.忽略内部元音
 print('/////////////////////////////////////////////')
-regexp=r'^[AEIOUaeiou]+|[AEIOUaeiou]+$|[^AEIOUaeiou]'
+regexp = r'^[AEIOUaeiou]+|[AEIOUaeiou]+$|[^AEIOUaeiou]'
+
+
 def compress(word):
-    pieces=re.findall(regexp,word)
+    pieces = re.findall(regexp, word)
     return ''.join(pieces)
 
-english_udhr=nltk.corpus.udhr.words('English-Latin1')
-x2=nltk.tokenwrap( compress(w) for w in english_udhr[:75] )
+
+english_udhr = nltk.corpus.udhr.words('English-Latin1')
+x2 = nltk.tokenwrap(compress(w) for w in english_udhr[:75])
 print(x2)
 
+# 3.提取辅音元音序列
+print('///////////////////////////////////////////////')
+rotokas_words = nltk.corpus.toolbox.words('rotokas.dic')
+cvs = [cv for w in rotokas_words for cv in re.findall(r'[ptksvr][aeiou]', w)]
+cfd = nltk.ConditionalFreqDist(cvs)
+cfd.tabulate()
 
-
-
-
-
+# 4.nltk.Index()转为有用索引
+print('///////////////////////////////////////////////')
+cv_word_pairs = [(cv, w) for w in rotokas_words for cv in re.findall(r'[ptksvr][aeiou]', w)]
+cv_index = nltk.Index(cv_word_pairs)
+print(cv_index['ka'])
