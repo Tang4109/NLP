839d3ec221919cdf02fa8b4ac209b327cd2b28eb
Tang4109 <2298964704@qq.com>
2019-11-22 21:59:05 +0800

    Brill标注器

---
+++ 5.6 基于转换的标注.py
@@ -0,0 +1,13 @@
+'''
+!/usr/bin/env python
+ _*_coding:utf-8 _*_
+@Time    :2019/11/22 21:43
+@Author  :Zhangyunjia
+@FileName: 5.6 基于转换的标注.py
+@Software: PyCharm
+'''
+from __future__ import division
+import nltk, re, pprint
+
+# Brill标注器
+
