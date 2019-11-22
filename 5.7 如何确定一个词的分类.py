9b1bec41a54da65b5cf80985472e995fd1a129b5
Tang4109 <2298964704@qq.com>
2019-11-22 22:59:47 +0800

    如何确定一个词的分类

---
+++ 5.7 如何确定一个词的分类.py
@@ -0,0 +1,14 @@
+'''
+!/usr/bin/env python
+ _*_coding:utf-8 _*_
+@Time    :2019/11/22 21:59
+@Author  :Zhangyunjia
+@FileName: 5.7 如何确定一个词的分类.py
+@Software: PyCharm
+'''
+from __future__ import division
+import nltk, re, pprint
+
+
+
+
