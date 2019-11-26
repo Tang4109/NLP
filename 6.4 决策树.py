67db664e9ed1bcd5160438ba0fd545fc86dd9258
Tang4109 <2298964704@qq.com>
2019-11-26 12:02:00 +0800

    决策树

---
+++ 6.4 决策树.py
@@ -0,0 +1,28 @@
+'''
+!/usr/bin/env python
+ _*_coding:utf-8 _*_
+@Time    : 2019/11/26 10:38
+@Author  :Zhangyunjia
+@FileName:  6.4 决策树.py
+@Software: PyCharm
+'''
+import nltk
+# 1.计算标签链表的熵
+import math
+
+
+def entropy(labels):
+    freqdist = nltk.FreqDist(labels)
+
+    # print('freqdist.items(): ', freqdist.items())
+    # print("freqdist['male']: ", freqdist['male'])
+
+    probs = [freqdist.freq(l) for l in nltk.FreqDist(labels)]
+    # print('probs: ', probs)
+    result = -sum([p * math.log(p, 2) for p in probs])
+    # print('result: ',result)
+    return result
+
+
+entropy_ = entropy(['fmale', 'female', 'male', 'xmale'])
+print('entropy_: ',entropy_)
